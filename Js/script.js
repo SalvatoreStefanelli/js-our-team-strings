761750a9166bd165fa2da456d98f478f090f2c5c
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-09 22:34:40 +0100

    create array

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+// MILESTONE 0:
+// Creare l’array di stringhe contenente i nomi dei membri del team.
+
+const name = ['Salvatore', 'Fabio', 'Gabriele', 'Alessio', 'Giovanni'];
+
+// MILESTONE 1:
+// Stampare le informazioni su DOM come card.
+
+
+// MILESTONE 3:
+// Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+
+// BONUS 1:
+// In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+
+// BONUS 2:
+// Inserire un form per l’aggiunta di un elemento alla lista.
+
